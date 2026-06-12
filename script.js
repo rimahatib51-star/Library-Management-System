cad802ed3d66cbf104f180a5601c3c5e4977526a
rimahatib51-star <rimahatib51@gmail.com>
2026-06-12 20:33:11 +0300

    Add showMessage function to display project info

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Bu proje Python console uygulamasıdır. Çalıştırmak için library_system.py dosyasını indirip Python ile açmalısınız.";
+}
